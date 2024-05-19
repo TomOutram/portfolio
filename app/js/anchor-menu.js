d2df20d6b7902bc35ab5c294bb93958c0605b238
TomOutram <tjoutram@gmail.com>
2024-05-19 19:21:15 +1200

    added dynamic anchor link menu enhancement

--- app/js/anchor-menu.js
+++ app/js/anchor-menu.js
@@ -1,26 +1,55 @@
+// get text blocks
+const allTextBlocks = document.querySelectorAll(".project-article__text-block");
+
 // get anchor menu items
-const anchorItems = document.querySelectorAll(
+const allAnchorItems = document.querySelectorAll(
   ".project-article__anchor-menu-item"
 );
 
-// add click listener to each menu item
-// on click - run 'selectAnchorItem' function to toggle 'selected' class
-for (let item of anchorItems) {
-  item.addEventListener("click", (e) => selectAnchorItem(e));
+// ----------------------------------------------------------------------
+// function to toggle the 'selected' class on anchor menu items
+// ----------------------------------------------------------------------
+function setSelectedAnchor(anchorItem) {
+  // remove 'selected' class from item that is currently selected
+  document
+    .querySelector(".project-article__anchor-menu-item--selected")
+    .classList.remove("project-article__anchor-menu-item--selected");
+  // add 'selected' class to the the item that is passed in
+  anchorItem.classList.toggle("project-article__anchor-menu-item--selected");
 }
 
-// loop through all menu items - remove the 'selected' class
-// toggle 'selected' class on the clicked menu item
-function selectAnchorItem(e) {
-  for (let item of anchorItems) {
-    item.classList.remove("project-article__anchor-menu-item--selected");
-  }
-  e.target.classList.toggle("project-article__anchor-menu-item--selected");
+// ----------------------------------------------------------------------
+// check scroll position and set the current text block the user is on
+// ----------------------------------------------------------------------
+// set the current text block (1 to start with)
+let currentTextBlock = "textBlock-1";
+// add listener for scroll
+window.addEventListener("scroll", () => {
+  // check if scrolled past the top of a text block
+  allTextBlocks.forEach((textBlock) => {
+    if (window.scrollY >= textBlock.offsetTop - 21) {
+      // if scrolled past a text block, change the current text block
+      currentTextBlock = textBlock.id;
+    }
+  });
+  // loop through all anchor menu items
+  allAnchorItems.forEach((anchorItem) => {
+    // if the anchor item matches the current section, call the function to toggle 'selected' class
+    if (anchorItem.dataset.anchorLink == currentTextBlock) {
+      setSelectedAnchor(anchorItem);
+    }
+  });
+});
 
-  // scroll to corresponding heading on page
-  scrollToTextblock(e.target.dataset.anchorLink);
+// ----------------------------------------------------------------------
+// when an anchor item is clicked - scroll to that text block
+// ----------------------------------------------------------------------
+// add click listeners to anchor items
+for (let item of allAnchorItems) {
+  item.addEventListener("click", (e) =>
+    scrollToTextblock(e.target.dataset.anchorLink)
+  );
 }
-
 // scroll funtion
 function scrollToTextblock(textBlockId) {
   // get the text block that corresponds to the anchor menu item
